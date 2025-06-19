a5118f54db3d9c4c67fb1992fec03eb89f956c34
youssef kadri <youssefkadri1818@gmail.com>
2025-06-19 12:52:35 +0200

    fix utc x7

--- backend-app/src/utils/date.ts
+++ backend-app/src/utils/date.ts
@@ -1,14 +1,10 @@
 // Fonction pour convertir l'heure de Paris en UTC
 export function parisToUTC(timeSlot: string, date: string) {
-    const [hours, minutes] = timeSlot.split(':').map(Number);
-    console.log("hours", hours);
-    console.log("minutes", minutes);
-    
-    // Créer une date avec l'heure de Paris
-    const parisDate = new Date(`${date.split('T')[0]}T${timeSlot}:00`);
-    console.log("parisDate", parisDate);
-    // La date créée est en heure locale (Paris), on la convertit en UTC
-    const utcDate = new Date(parisDate.getTime() - (parisDate.getTimezoneOffset() * 60000));
-    console.log("utcDate", utcDate);
+    const dateOnly = date.split('T')[0]; // "2025-06-25"
+    const dateParis = new Date(`${dateOnly}T${timeSlot}:00+02:00`); // UTC+2
+    const utcDate = new Date(dateParis.toISOString());
+
+    console.log('→ utcDate', utcDate.toISOString());
     return utcDate;
+
 }
