cc6f51437d6726e0aba2b0a0f8d0f2b9025532bb
Viturna <148191882+Viturna@users.noreply.github.com>
2025-07-07 15:45:10 +0200

    Cred

--- index.js
+++ index.js
@@ -21,11 +21,15 @@ const authenticate = async (req, res, next) => {
 };
 
 // Firebase Admin SDK init
-const serviceAccount = require('./serviceAccountKey.json');
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-});
-
+if (!admin.apps.length) {
+    admin.initializeApp({
+        credential: admin.credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        }),
+    });
+}
 const db = admin.firestore();
 
 // Route sécurisée
